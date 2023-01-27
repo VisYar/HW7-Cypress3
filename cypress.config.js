d04a2f3aa265fa9a7a306706572e486edca3a9bd
Tatyana Dmitrieva <visyar76@gmail.com>
2023-01-28 01:47:46 +0300

    Connecting a project to Dashboard

--- cypress.config.js
+++ cypress.config.js
@@ -1,7 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  e2e: {
+	e2e: {
+		projectId: "z49zox",
     baseUrl: "https://petstore.swagger.io/v2",
     include: "cypress/e2e/apiTest/*.cy.{js,jsx,ts,tsx}",
     retries: {
